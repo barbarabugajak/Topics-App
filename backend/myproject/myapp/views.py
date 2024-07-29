bc6e9f5470763dc6682bb6476ffee277d5e74b0d
Coder <basia.bugi.coding@icloud.com>
2024-07-29 11:20:31 +0200

    Implement random topic generator (API)

--- backend/myproject/myapp/views.py
+++ backend/myproject/myapp/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Category, Topic
 from .serializers import CategorySerializer, TopicSerializer
+from .scraper import scrape
+import random
 
 class TopicView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
 
 def getRandomTopicAPI(request):
-    all_topics = Topic.objects.all()
     scrape()
-    
+    all_topics = Topic.objects.all()
+    random_topic = random.choice(all_topics)
+    serializer = TopicSerializer(random_topic)
+    return Response(serializer.data)
+
 
