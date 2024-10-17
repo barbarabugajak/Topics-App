5cdc50dffd930e2fd1ba12a875d02fac26059aaa
Coder <basia.bugi.coding@icloud.com>
2024-10-17 17:01:23 +0200

    Sync

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,54 +1,70 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React from 'react';
+import { Image, StyleSheet, Platform, View, Text, Button, Pressable, Linking} from 'react-native';
+import axios from 'axios';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+  const [topic, setTopic] = React.useState(null);
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+          {topic &&  
+          <>
+          <Text style={{
+            fontSize: 20,
+            marginBottom: 8,
+            }}>
+              Category: {topic["category"]["name"]}
+            </Text>
+
+          {topic["link"] && 
+            <Text style={{
+              fontSize: 16,
+              marginBottom: 8,
+              // Make it look like a link
+              color: 'blue',
+              textDecorationLine: 'underline',
+            }}
+            onPress={() =>
+              Linking.openURL(topic["link"])
+            }>
+              Link to some information about the topic
+            </Text>
+          }
+
+          <Text style={{
+          fontSize: 24,
+          marginBottom: 8,
+          // Make bold
+          fontWeight: 'bold',
+          }}>
+            {topic["name"]}
+          </Text>
+          </>
+          }
+        <Pressable
+          onPress={() => {
+            axios.get('http://127.0.0.1:8000/api/random_topic').then((response) => {
+              console.log(response.data);
+              setTopic(response.data);
+            });
+          }}
+          style={{
+            backgroundColor: '#2196F3',
+            padding: 10,
+            borderRadius: 5,
+          }}
+        >
+          <Text>Choose a{topic && "nother"} random topic</Text>
+        </Pressable>
+      </View>
+    );
 }
+
 
 const styles = StyleSheet.create({
   titleContainer: {
